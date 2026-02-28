b3bb651b66e23d06fbca65d08419017e35e24cdc
RezThePerson <rhythmupa@gmail.com>
2026-02-28 13:08:20 +0530

    add some logic

--- src/main.js
+++ src/main.js
@@ -7,5 +7,12 @@ export const submarine = k.add([k.pos(120, 80), k.sprite("submarine")]);
 
 import { movement } from "./lib/movement.js";
 movement();
-// import { enemy } from "./lib/enemy.js";
-// enemy();
+import { loadEnemies } from "./lib/enemies/loadEnemies.js";
+loadEnemies();
+
+import { game } from "./store.js";
+k.onUpdate(() => {
+    game.level += 0.1;
+
+    console.log(Math.floor(game.level));
+});
---
+++ src/store.js
@@ -0,0 +1,1 @@
+export let game = { level: 0 };
