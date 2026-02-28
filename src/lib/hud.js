ed2ca27684ded30966fc46f9947d08eb096f89a6
RezThePerson <reztheperson@proton.me>
2026-02-28 22:50:49 +0530

    yes :3

--- src/lib/hud.js
+++ src/lib/hud.js
@@ -1,6 +1,23 @@
 import { k } from "../main.js";
 import { gameState } from "../store.js";
 
+function background() {
+    k.loadSprite("background", "sprites/background.png", {
+        sliceX: 4,
+        sliceY: 2,
+        anims: {
+            idle: {
+                from: 0,
+                to: 7,
+                loop: true,
+                speed: 5,
+            },
+        },
+    });
+
+    k.add([k.fixed(), k.sprite("background", { anim: "idle" }), k.z(-100)]);
+}
+
 function depth() {
     const depthLabel = k.add([
         k.text("0 ft", { size: 48 }),
@@ -16,5 +33,6 @@ function depth() {
 }
 
 export function loadHUD() {
+    background();
     depth();
 }
